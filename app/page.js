587d11354c4caf85b3da97e2355cc5d604641ae8
MacBook <apple@Quadrys-MacBook-Pro.local>
2023-08-31 11:35:57 +0100

    second phase updates and functionaity

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-import Link from "next/link";
 import UrlShortner from "./urlshortner/page";
 
 export default function Home() {
   return (
-    <main className="text-center">
+    <main className="text-center overscroll-none">
       <UrlShortner />
     </main>
   );
--- app/urlshortner/page.js
+++ app/urlshortner/page.js
@@ -1,9 +1,14 @@
 "use client";
 import React, { useState } from "react";
-
+import { BsMoon, BsSun } from "react-icons/bs";
+import { Open_Sans } from "next/font/google";
+const opensans = Open_Sans({ subsets: ["latin"], weight: ["400", "700"] });
 export default function UrlShortner() {
   const [link, setLink] = useState("");
   const [shortenedLink, setShortenedLink] = useState("");
+  const [theme, setTheme] = useState(<BsMoon />);
+  const [background, setBackground] = useState("bg-orange-50");
+  const [theColor, setThecolor] = useState("i");
 
   const Shorten = async () => {
     let request = await fetch(
@@ -13,21 +18,51 @@ export default function UrlShortner() {
     let shortened = response.result.full_short_link;
     setShortenedLink(shortened);
   };
+  const DarkTheme = (e) => {
+    if (background === "bg-orange-50") {
+      setBackground("bg-slate-800");
+      setTheme(<BsSun className="text-white" />);
+      setThecolor("text-white");
+    } else {
+      setBackground("bg-orange-50");
+      setTheme(<BsMoon />);
+      setThecolor("t");
+    }
+    e.preventDefault();
+  };
 
   return (
-    <div className="text-center py-80 h-full bg-orange-50">
-      <input
-        onChange={(e) => setLink(e.target.value)}
-        className=" border-x-0 rounded bg-gray-300 mr-2 h-10 w-80 sm:mb-6"
-        type="text"
-      ></input>
-      <button
-        className=" text-white bg-blue-400 rounded p-3  "
-        onClick={Shorten}
-      >
-        Submit
-      </button>
-      <p className="mt-4">{shortenedLink}</p>
+    <div style={opensans.style} className={`pt-40 pb-96 ${background}`}>
+      <div className="w-8/12  m-auto">
+        <button
+          onClick={DarkTheme}
+          className="  absolute right-28 top-20 text-4xl"
+        >
+          {theme}
+        </button>
+        <h1 className={`text-4xl font-bold mb-2 ${theColor}`}>
+          Simplify links with our URL shortener
+        </h1>
+        <h5 className={`text-lg ${theColor}`}>
+          condense lengthy URLs into concise, shareable links for effortless
+          navigation.
+        </h5>
+      </div>
+      <div className="mt-16">
+        <input
+          onChange={(e) => setLink(e.target.value)}
+          className=" border-slate-600 border-2 rounded bg-gray-50 mr-2 h-10 w-80 sm:mb-6"
+          type="text"
+        ></input>
+        <button
+          className=" text-white bg-blue-400 rounded p-3  "
+          onClick={Shorten}
+        >
+          Submit
+        </button>{" "}
+        <br />
+        <p className="mt-4 ">{shortenedLink}</p>
+      </div>
     </div>
   );
 }
