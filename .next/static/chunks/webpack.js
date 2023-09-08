35806e3acd8522d72eb0494109e3c03bb12c100f
MacBook <apple@Quadrys-MacBook-Pro.local>
2023-09-08 15:29:22 +0100

    head updates

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ce194715ae51b2a5"; }
+/******/ 		__webpack_require__.h = function() { return "9b0c442725c1b61c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
+++ .next/static/webpack/webpack.08bbe3671c2a994f.hot-update.js
@@ -96,7 +96,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "2542ad81c90afb99"; }
+/******/ 	__webpack_require__.h = function() { return "606c42ea1a6e868e"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
